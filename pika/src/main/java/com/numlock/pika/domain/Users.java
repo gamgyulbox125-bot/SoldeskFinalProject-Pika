3af2df3539af7058605cfab409937318929b37e8
BJK25 <rlqjawns10@gmail.com>
2025-12-16 12:18:16 +0900

    비밀번호 암호화, UserController->WishListController로 변경, 새 UserController 추가(Update,Delete관리)

--- pika/src/main/java/com/numlock/pika/domain/Users.java
+++ pika/src/main/java/com/numlock/pika/domain/Users.java
@@ -4,9 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -23,24 +20,17 @@ public class Users {
     //PK
     @Id
     @Column(nullable = false)
-    @NotBlank(message = "아이디를 입력하세요.")
-    @Pattern(regexp = "^[a-zA-Z0-9]{4,20}$", message = "아이디는 4~20자의 알파벳과 숫자만 사용 가능합니다.")
     private String id;
 
     //필수 입력값
-    @Column(nullable = false)
-    @NotBlank(message = "비밀번호를 입력하세요.")
-    @Pattern(regexp = "^[a-zA-Z0-9]{4,20}$", message = "비밀번호는 4~20자의 알파벳과 숫자만 사용 가능합니다.")
+    @Column(nullable = false, length = 255)
     private String pw;
 
     @Column(nullable = false)
-    @NotBlank(message = "닉네임을 입력하세요.")
     private String nickname;
 
 
     @Column(nullable = false)
-    @NotBlank(message = "이메일 주소를 입력하세요.")
-    @Email(message = "잘 못 된 이메일 형식입니다.")
     private String email;
 
     //추가 입력값
@@ -67,13 +57,4 @@ public class Users {
         this.email = email;
         this.role = role;
     }
-
-    public Users updateUserRole(String address, String phone, Date birth){
-        this.address = address;
-        this.phone = phone;
-        this.birth = birth;
-        this.role = "USER";
-        return this;
-    }
-
 }
