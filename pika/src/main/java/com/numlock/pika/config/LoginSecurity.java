1b7b420c8988f4d07dd8b84e76df05dee86e971e
BJK25 <rlqjawns10@gmail.com>
2025-12-19 16:58:06 +0900

    시큐리티 권한 해제

--- pika/src/main/java/com/numlock/pika/config/LoginSecurity.java
+++ pika/src/main/java/com/numlock/pika/config/LoginSecurity.java
@@ -28,14 +28,14 @@ public class LoginSecurity {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorize -> authorize
+                /*.authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/css/**", "/js/**", "/icon/**", "/upload/**", "/profile/**").permitAll()
                         .requestMatchers("/", "/user/login", "/user/join", "/search/**", "/products/info/**").permitAll()
                         //마이페이지, 상품등록 가입사용자 모두 접근 가능(USER/GUEST 권환 분리 논의 필요)
                         .requestMatchers("/user/mypage/**", "/products/new").hasAnyRole("USER", "GUEST", "ADMIN")
                         .anyRequest().authenticated() //나머지는 인증 필요
-                )
-                //.authorizeHttpRequests(authorize -> authorize.requestMatchers("/**").permitAll())
+                )*/
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers("/**").permitAll())
                 // 모든 경로에 대한 접근 허용 (개발용) 개발 완료후 삭제 필요
                 .formLogin(form -> form.loginPage("/user/login") //사용자 정의 로그인 페이지
                         .usernameParameter("id") //loginForm의 username을 id로
